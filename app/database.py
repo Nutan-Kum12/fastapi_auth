780f91896f14384ba132a3073a3ed59f14f87974
Nutan-Kum12 <166277808+Nutan-Kum12@users.noreply.github.com>
2025-05-31 00:13:28 +0530

    Push whole file

---
+++ app/database.py
@@ -0,0 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")  # e.g. mongodb+srv://username:password@cluster0.mongodb.net/dbname
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["auth_db"]
+user_collection = db["users"]
