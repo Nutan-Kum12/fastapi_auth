c571619825f08d19eb0cad2ecdeed89b5b24f3c6
Nutan-Kum12 <166277808+Nutan-Kum12@users.noreply.github.com>
2025-05-31 01:53:47 +0530

    Login method fix

--- app/auth.py
+++ app/auth.py
@@ -22,15 +22,24 @@ async def signup(user: UserCreate):
     return ShowUser(name=user.name, email=user.email, mobile=user.mobile)
 
 
-# Login
 @router.post("/login", response_model=Token)
 async def login(user: UserLogin):
+    print("Login attempt for:", user.email)
     db_user = await user_collection.find_one({"email": user.email})
-    if not db_user or not verify_password(user.password, db_user["hashed_password"]):
+    if not db_user:
+        print("User not found")
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+    
+    print("Found user:", db_user)
+    is_verified = verify_password(user.password, db_user.get("hashed_password", ""))
+    print("Password verified:", is_verified)
+
+    if not is_verified:
         raise HTTPException(status_code=400, detail="Invalid credentials")
 
     token = create_access_token(data={"email": user.email})
     return {"access_token": token, "token_type": "bearer"}
+
 
 
 # Get logged-in user details
